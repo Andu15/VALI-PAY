a2fcfb479714a2a06eeb3dd19eb426c1c84ad6d0
Andu15 <estefania_8_3@hotmail.com>
2022-06-01 23:30:33 -0500

    add initial structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
+// sections
 const actionPage = document.querySelector('#action-page');
+const formPage = document.querySelector('#form-page');
+const resultPage = document.querySelector('#result-page');
+
+// btns
+const btnPay = document.querySelector('#btn-pay');
+
+// actions
+function toggleElement(target1, target2, target3) {
+  target1.classList.toggle('hidden');
+  target2.classList.toggle('block');
+  target3.classList.toggle('block');
+}
+
+// events
+btnPay.addEventListener('click', () => toggleElement(actionPage, formPage, resultPage));
